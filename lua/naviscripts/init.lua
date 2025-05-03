5cc8438b1f4e0054209c4edbe7b0f4daad45c8a5
Ed Sweeney <ed@onextent.com>
2025-05-03 07:59:33 -0700

    help

--- lua/naviscripts/init.lua
+++ lua/naviscripts/init.lua
@@ -7,6 +7,11 @@ function M.setup()
 	vim.api.nvim_create_user_command("FormatRustFiles", function()
 		M.format_rust_files()
 	end, { desc = "Format all Rust files in the src/ directory and subdirectories" })
+
+	-- Load the help file
+	local plugin_dir = vim.fn.stdpath("data") .. "/lazy/nvim-naviscripts"
+	local doc_dir = plugin_dir .. "/doc"
+	vim.cmd("helptags " .. doc_dir)
 end
 
 return M
