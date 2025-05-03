97a855ad8ad5af03403d56b04b6d79d21227b17d
Ed Sweeney <ed@onextent.com>
2025-05-03 07:39:52 -0700

    setup

--- lua/naviscripts/init.lua
+++ lua/naviscripts/init.lua
@@ -2,11 +2,11 @@ local M = {}
 
 M.format_rust_files = require("naviscripts.format_rust").format_rust_files
 
--- Register the :FormatRustFiles command
-vim.api.nvim_create_user_command("FormatRustFiles", function()
-	M.format_rust_files()
-end, { desc = "Format all Rust files in the src/ directory and subdirectories" })
-
-function M.setup(user_config) end
+function M.setup()
+	-- Register the :FormatRustFiles command
+	vim.api.nvim_create_user_command("FormatRustFiles", function()
+		M.format_rust_files()
+	end, { desc = "Format all Rust files in the src/ directory and subdirectories" })
+end
 
 return M
